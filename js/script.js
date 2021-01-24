25d2798a55111a221209cb809dffaa5e69650c0c
Aidarbek <aidarik.jazik@gmail.com>
2021-01-25 02:37:29 +0600

    modified modal windows by setTimeOut and scroll

--- js/script.js
+++ js/script.js
@@ -98,15 +98,16 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
       modal = document.querySelector('.modal'),
       modalCloseBtn = document.querySelector('[data-close]');
 
-      
+      function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearTimeout(modalTimerId);
+      }
 
       modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-          modal.classList.add('show');
-          modal.classList.remove('hide');
-          document.body.style.overflow = 'hidden';
-        })
-       } );
+        btn.addEventListener('click', openModal);
+       });
 
       function closeModal() {
         modal.classList.add('hide');
@@ -127,4 +128,17 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
           closeModal();
         }
       });
+
+      const modalTimerId = setTimeout(openModal, 5000);
+
+      function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+          openModal();
+          window.removeEventListener('scroll', showModalByScroll);
+      }
+    }
+     
+    window.addEventListener('scroll', showModalByScroll);
+
+
 });
